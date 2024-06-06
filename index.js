cefd34af3ff8eba7498460c9c78ebe725ba74fa7
Aditya Fathan <108979697+adtyfathan@users.noreply.github.com>
2024-06-06 21:31:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+import { data } from "./datas.js";
+
 let activeSlide = 2;
 
+// console.log(data);
+
+data.map(x => {
+    console.log(x.model);
+});
+
 setSlideHeader();
+
+document.getElementById("sub-slide").addEventListener("click", subtSlide);
+document.getElementById("add-slide").addEventListener("click", addSlide);
 
 function addSlide() {
     document.querySelectorAll("[data-index]")[activeSlide].classList.remove("active");
@@ -18,13 +29,13 @@ function subtSlide() {
 
 function setSlideHeader() {
     if (activeSlide == 0) {
-        document.querySelector(".slide-header").innerHTML = "URUS"
-        document.querySelector(".slide-sub").innerHTML = "DARE TO LIVE MORE"
+        document.querySelector(".slide-header").innerHTML = "PORSCHE 911"
+        document.querySelector(".slide-sub").innerHTML = "PORSCHE"
     } else if (activeSlide == 1) {
-        document.querySelector(".slide-header").innerHTML = "HURACAN"
-        document.querySelector(".slide-sub").innerHTML = "BEYOND THE CONCRETE"
+        document.querySelector(".slide-header").innerHTML = "F8 TRIBUTO"
+        document.querySelector(".slide-sub").innerHTML = "FERRARI"
     } else if (activeSlide == 2) {
         document.querySelector(".slide-header").innerHTML = "REVUELTO"
-        document.querySelector(".slide-sub").innerHTML = "FROM NOW ON"
+        document.querySelector(".slide-sub").innerHTML = "LAMBORGHINI"
     }
 }
