af7cca4509e103fc1f4ff689516b2798da73562a
DzakwanGoreng <161908992+DzakwanGoreng@users.noreply.github.com>
2024-06-12 20:04:49 +0700

    Add files via upload

--- datas.js
+++ datas.js
@@ -38,7 +38,6 @@ const data = [
     power: "650 CV",
     max_speed: "305km/h",
     acceleration: "3.6s",
-    image: "./asset/urus.jpeg",
     description:
       "The Urus is Lamborghini's first Super SUV, merging the soul of a super sports car with the functionality of an SUV. Equipped with a 4.0-liter twin-turbo V8 engine, it delivers exceptional performance, luxury, and versatility. The Urus is designed to provide an exhilarating driving experience on any terrain.",
     price: "$218,000",
@@ -49,7 +48,6 @@ const data = [
     power: "819 CV",
     max_speed: "350km/h",
     acceleration: "2.8s",
-    image: "./asset/sian.jpg",
     description:
       "The Sian FKP 37 is Lamborghini's first hybrid super sports car, featuring a groundbreaking combination of a V12 engine and a 48-volt e-motor. This limited-edition model showcases Lamborghini's commitment to innovation and sustainability while delivering unmatched power and performance. The Sian's futuristic design and advanced technology set a new standard in the world of supercars.",
     price: "$3,700,000",
@@ -60,7 +58,6 @@ const data = [
     power: "570 CV",
     max_speed: "325km/h",
     acceleration: "3.4s",
-    image: "./asset/gallardo.jpg",
     description:
       "The Gallardo LP 570-4 Superleggera is a lightweight, high-performance variant of the Gallardo. It features a 5.2-liter V10 engine, advanced aerodynamics, and extensive use of carbon fiber, making it one of the most agile and responsive Lamborghinis ever built.",
     price: "$237,600",
@@ -71,7 +68,6 @@ const data = [
     power: "670 CV",
     max_speed: "342km/h",
     acceleration: "3.2s",
-    image: "./asset/murcielago.jpg",
     description:
       "The Murciélago LP 670-4 SV (SuperVeloce) is the ultimate version of the Murciélago, boasting increased power, reduced weight, and enhanced aerodynamics. With its 6.5-liter V12 engine and aggressive design, it delivers a raw and exhilarating driving experience.",
     price: "$450,000",
@@ -82,7 +78,6 @@ const data = [
     power: "750 CV",
     max_speed: "355km/h",
     acceleration: "2.8s",
-    image: "./asset/veneno.jpeg",
     description:
       "The Lamborghini Veneno is a limited-production hypercar built to celebrate Lamborghini's 50th anniversary. With its radical design, extreme performance, and a 6.5-liter V12 engine, the Veneno is one of the most exclusive and sought-after Lamborghinis ever made.",
     price: "$4,500,000",
@@ -93,7 +88,6 @@ const data = [
     power: "770 CV",
     max_speed: "350km/h",
     acceleration: "2.8s",
-    image: "./asset/centenario.jpg",
     description:
       "The Centenario was created to honor the 100th birthday of Lamborghini's founder, Ferruccio Lamborghini. It features advanced aerodynamics, cutting-edge technology, and a powerful V12 engine. The Centenario is a true collector's item, with only 40 units produced.",
     price: "$2,000,000",
@@ -104,7 +98,6 @@ const data = [
     power: "650 CV",
     max_speed: "340km/h",
     acceleration: "3.4s",
-    image: "./asset/reventon.jpg",
     description:
       "The Lamborghini Reventón is an ultra-exclusive model inspired by the fastest airplanes. With its stealth-fighter design, carbon fiber body, and 6.5-liter V12 engine, the Reventón delivers exceptional performance and an unmatched visual presence.",
     price: "$2,000,000",
@@ -115,7 +108,6 @@ const data = [
     power: "530 CV",
     max_speed: "335km/h",
     acceleration: "3.9s",
-    image: "./asset/diablo.jpg",
     description:
       "The Diablo SV (SuperVeloce) is a high-performance variant of the iconic Diablo. With a 5.7-liter V12 engine and rear-wheel drive, the Diablo SV offers an exhilarating driving experience with distinctive styling and raw power.",
     price: "$300,000",
@@ -126,7 +118,6 @@ const data = [
     power: "814 CV",
     max_speed: "355km/h",
     acceleration: "2.8s",
-    image: "./asset/countach.jpg",
     description:
       "The Countach LPI 800-4 is a modern interpretation of the classic Countach, celebrating its legacy with advanced hybrid technology and a powerful V12 engine. With its iconic design and cutting-edge performance, the Countach LPI 800-4 bridges the past and future of Lamborghini.",
     price: "$2,640,000",
@@ -137,7 +128,6 @@ const data = [
     power: "640 CV",
     max_speed: "310km/h",
     acceleration: "3.0s",
-    image: "./asset/sto.jpg",
     description:
       "The Huracán STO (Super Trofeo Omologata) is a track-focused variant of the Huracán, designed for maximum performance and agility. With a naturally aspirated V10 engine and advanced aerodynamics, the Huracán STO delivers an unparalleled driving experience on both road and track.",
     price: "$327,838",
@@ -148,7 +138,6 @@ const data = [
     power: "830 CV",
     max_speed: "N/A",
     acceleration: "N/A",
-    image: "./asset/scv.jpg",
     description:
       "The Essenza SCV12 is a track-only hypercar with the most powerful V12 engine ever developed by Lamborghini. It features advanced aerodynamics and lightweight construction, designed exclusively for high-performance driving on the track.",
     price: "$2,700,000",
@@ -159,7 +148,6 @@ const data = [
     power: "819 CV",
     max_speed: "350km/h",
     acceleration: "2.9s",
-    image: "./asset/sian-roadster.jpg",
     description:
       "The Sián Roadster combines Lamborghini's legendary V12 engine with hybrid technology, offering a unique open-top driving experience. With its futuristic design and innovative powertrain, the Sián Roadster represents the pinnacle of Lamborghini's engineering and performance.",
     price: "$3,700,000",
@@ -170,7 +158,6 @@ const data = [
     power: "350 CV",
     max_speed: "260km/h",
     acceleration: "6.8s",
-    image: "./asset/jarama.jpg",
     description:
       "The Lamborghini Jarama GT is a classic grand tourer from the 1970s, featuring a 3.9-liter V12 engine and distinctive styling. Known for its luxurious interior and refined performance, the Jarama GT is a testament to Lamborghini's heritage of elegance and power.",
     price: "$350,000",
@@ -181,7 +168,6 @@ const data = [
     power: "385 CV",
     max_speed: "290km/h",
     acceleration: "6.7s",
-    image: "./asset/miura.jpg",
     description:
       "The Miura P400 SV is an iconic supercar from the 1970s, often considered the first true supercar. With its mid-engine layout and stunning design, the Miura SV set new standards for performance and aesthetics in the automotive world.",
     price: "$3,000,000",
@@ -280,6 +266,7 @@ const data = [
     power: "761 PS",
     max_speed: "260km/h",
     acceleration: "2.8s",
+	image: "./asset/Taycan Turbo S.jpg",
     description:
       "The Porsche Taycan Turbo S represents the future of electric sports cars. With an all-electric powertrain, the Taycan Turbo S combines Porsche's legendary performance with cutting-edge technology. It features a dual-motor setup that produces 761 PS and can accelerate from 0 to 100 km/h in just 2.8 seconds. The Taycan Turbo S also boasts advanced driving dynamics and a luxurious interior, making it a perfect blend of innovation and tradition. The car's top speed is 260 km/h, and it offers an unparalleled driving experience, proving that electrification does not compromise the thrill of driving a Porsche.",
     price: "$185,000",
@@ -290,6 +277,7 @@ const data = [
     power: "520 hp",
     max_speed: "312 km/h",
     acceleration: "3.0s",
+	image: "./asset/911 GT3 RS.jpeg",
     description:
       "The Porsche 911 GT3 RS is a track-focused version of the iconic 911 sports car. It is powered by a naturally aspirated 4.0-liter flat-six engine that produces 520 horsepower. With a top speed of 312 km/h and a 0-100 km/h acceleration time of just 3.0 seconds, the 911 GT3 RS delivers exhilarating performance on both the road and the track. Its aerodynamic enhancements, lightweight construction, and precision-tuned chassis make it a favorite among driving enthusiasts seeking the ultimate Porsche experience.",
     price: "$188,000",
@@ -300,6 +288,7 @@ const data = [
     power: "680 hp",
     max_speed: "295 km/h",
     acceleration: "3.8s",
+	image: "./asset/Cayenne Turbo S E-Hybrid.png",
     description:
       "The Porsche Cayenne Turbo S E-Hybrid is a high-performance SUV that combines electrification with Porsche's renowned performance capabilities. It features a hybrid powertrain consisting of a twin-turbo V8 engine and an electric motor, producing a combined output of 680 horsepower. With a top speed of 295 km/h and a 0-100 km/h acceleration time of 3.8 seconds, the Cayenne Turbo S E-Hybrid offers impressive performance for an SUV. Its luxurious interior, advanced technology features, and dynamic driving characteristics make it a versatile and desirable choice for those seeking a blend of power, efficiency, and comfort.",
     price: "$161,900",
@@ -310,6 +299,7 @@ const data = [
     power: "700 hp",
     max_speed: "315 km/h",
     acceleration: "3.2s",
+	image: "./asset/Panamera Turbo S E-Hybrid (1).png",
     description:
       "The Porsche Panamera Turbo S E-Hybrid is a luxurious and high-performance hybrid sedan. It combines a twin-turbo V8 engine with an electric motor to produce a total output of 700 horsepower. With a top speed of 315 km/h and a 0-100 km/h acceleration time of 3.2 seconds, the Panamera Turbo S E-Hybrid offers sports car performance in a spacious and comfortable package. Its advanced technology, elegant design, and dynamic driving characteristics make it a standout choice in the luxury sedan segment.",
     price: "$187,700",
@@ -320,6 +310,7 @@ const data = [
     power: "420 hp",
     max_speed: "304 km/h",
     acceleration: "4.4s",
+	image: "./asset/718 Cayman GT4.jpeg",
     description:
       "The Porsche 718 Cayman GT4 is a high-performance sports car designed for driving enthusiasts. Powered by a naturally aspirated flat-six engine, it produces 420 horsepower and delivers exhilarating performance on both road and track. With a top speed of 304 km/h and a 0-100 km/h acceleration time of 4.4 seconds, the 718 Cayman GT4 offers precise handling, superb agility, and a thrilling driving experience. Its lightweight construction, aerodynamic design, and racing-inspired features make it a favorite among Porsche enthusiasts.",
     price: "$141,700",
@@ -330,6 +321,7 @@ const data = [
     power: "440 hp",
     max_speed: "270 km/h",
     acceleration: "4.3s",
+	image: "./asset/Porsche macan.jpg",
     description:
       "The Porsche Macan Turbo is a compact luxury SUV that offers sporty performance and everyday practicality. Powered by a turbocharged V6 engine, it produces 440 horsepower and delivers dynamic driving characteristics. With a top speed of 270 km/h and a 0-100 km/h acceleration time of 4.3 seconds, the Macan Turbo combines Porsche's performance heritage with SUV versatility. Its stylish design, premium interior, and advanced technology features make it a desirable choice for those seeking a blend of sportiness and functionality.",
     price: "$83,600",
@@ -340,6 +332,7 @@ const data = [
     power: "640 hp",
     max_speed: "330 km/h",
     acceleration: "2.7s",
+	image: "./asset/911 Turbo S.jpg",
     description:
       "The Porsche 911 Turbo S is a high-performance sports car that combines luxury and speed. It features a twin-turbocharged flat-six engine producing 640 horsepower, enabling it to reach a top speed of 330 km/h and accelerate from 0 to 100 km/h in just 2.7 seconds. The 911 Turbo S offers exceptional handling, advanced technology, and a refined interior, making it a top choice for enthusiasts seeking both track performance and everyday comfort.",
     price: "$216,300",
@@ -350,6 +343,7 @@ const data = [
     power: "560 hp",
     max_speed: "250 km/h",
     acceleration: "3.3s",
+	image: "./asset/Taycan Cross Turismo.jpeg",
     description:
       "The Porsche Taycan Cross Turismo is an all-electric performance wagon that combines versatility with thrilling driving dynamics. With a dual-motor setup producing 560 horsepower, it can accelerate from 0 to 100 km/h in 3.3 seconds and reach a top speed of 250 km/h. The Cross Turismo offers ample cargo space, off-road capability, and the latest in electric vehicle technology, making it a versatile choice for those seeking an eco-friendly yet exciting driving experience.",
     price: "$150,900",
@@ -360,6 +354,7 @@ const data = [
     power: "400 hp",
     max_speed: "293 km/h",
     acceleration: "4.5s",
+	image: "./asset/718 Boxster GTS 4.0.jpeg",
     description:
       "The Porsche 718 Boxster GTS 4.0 is a mid-engine sports car that offers a perfect balance of performance and driving pleasure. Powered by a naturally aspirated flat-six engine producing 400 horsepower, it can reach a top speed of 293 km/h and accelerate from 0 to 100 km/h in 4.5 seconds. The Boxster GTS 4.0 features agile handling, a sleek design, and a convertible top, making it an ideal choice for open-air driving enthusiasts.",
     price: "$90,300",
@@ -370,6 +365,7 @@ const data = [
     power: "500 hp",
     max_speed: "320 km/h",
     acceleration: "3.7s",
+	image: "./asset/Cayman GT4 RS.jpg",
     description:
       "The Porsche Cayman GT4 RS is a track-focused sports car that pushes the boundaries of performance. With a 500-horsepower naturally aspirated engine, it can reach a top speed of 320 km/h and accelerate from 0 to 100 km/h in just 3.7 seconds. The Cayman GT4 RS features aerodynamic enhancements, lightweight construction, and precision engineering, delivering an exhilarating driving experience both on the track and the road.",
     price: "$141,700",
